5cb03e65b1685b59eb620d008c8653cd625c24de
lexamulat <lexamulat@mail.ru>
2018-04-15 21:27:40 +0300

    bg picture

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func mainhandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("static/start.html")
+	t, _ := template.ParseFiles("static/html/start.html")
 	t.Execute(w, t)
 }
 
 func main() {
+	router := mux.NewRouter()
+
+	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+	router.PathPrefix("/static/").Handler(s)
+
+	router.HandleFunc("/", mainhandler).Methods("GET")
 
 	fmt.Println("test")
-	http.HandleFunc("/", mainhandler)
-	log.Fatal(http.ListenAndServe(":9999", nil))
+	log.Fatal(http.ListenAndServe(":9999", router))
+
 }
