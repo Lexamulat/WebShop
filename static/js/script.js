ee02eeaef10884916ae8a266ff323a8327402a10
lexamulat <lexamulat@mail.ru>
2018-05-26 12:24:05 +0300

    getting content from db for all menus

--- static/js/script.js
+++ static/js/script.js
@@ -23,22 +23,62 @@ async function WriteBmenu(BMenu) {
     console.log("WriteBmenu");
     console.log(BMenu);
 
-    let list = $(".MenuList") // . means class | #means id
+    let list = $(".BurgsMenu") // . means class | #means id
+    list.empty();
+
+    for (let i = 0; i < BMenu.length; i++) {
+        const el = BMenu[i];
+        let listEl = `
+        <div class="card wow zoomIn" data-wow-duration="2s">
+                <img class="card-img-top " src="${el.ImgPath}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title ">${el.Name}</h5>
+                    <p class="card-text ">${el.Description}</p>
+                    <div class="redactbuttons">
+                        <a class=" mybtn btn btn-primary edit" data-toggle="modal" data-target="#editModal" data-id=${el.Id} data-name='${el.Name}' data-description='${el.Description}' data-img='${el.ImgPath}' >Edit</a>
+                        <a class="delete" data-toggle="modal" data-target="#exampleModal">Del</a>
+                    </div>
+                </div>
+            </div>
+        `
+
+
+        //     let listEl = `
+        //     <div class="card wow zoomIn" data-wow-duration="2s" >
+        //         <img class="card-img-top " src="${el.ImgPath}" alt="img">
+        //         <div class="card-body ">
+        //             <h5 class="card-title ">${el.Name}</h5>
+        //             <p class="card-text ">${el.Description}</p>
+        //             <a href="#" class="mybtn btn btn-primary" data-toggle="modal" data-target="#editModal" data-id=${el.Id} data-name='${el.Name}' data-description='${el.Description}' data-img='${el.ImgPath}' >Go somewhere</a>
+        //             </div>
+        //     </div>
+        //    `
+        // !! TAKE care of @data-name=${el.Name}@ and @data-name='${el.Name}'@  the difference in reading up to a blank in the first case. space after @data-name='${el.Name}'@ is very important
+        // !! data-'all in lowercase'
+        list.append(listEl)
+    }
+}
+
+async function WriteSubMenu(BMenu) {
+    console.log("WriteBmenu");
+    console.log(BMenu);
+
+    let list = $(".SubMenu") // . means class | #means id
     list.empty();
 
     for (let i = 0; i < BMenu.length; i++) {
         const el = BMenu[i];
 
         let listEl = `
-        <div class="card wow zoomIn" data-wow-duration="2s" >
-            <img class="card-img-top " src="${el.ImgPath}" alt="img">
-            <div class="card-body ">
-                <h5 class="card-title ">${el.Name}</h5>
-                <p class="card-text ">${el.Description}</p>
-                <a href="#" class="mybtn btn btn-primary" data-toggle="modal" data-target="#editModal" data-id=${el.Id} data-name='${el.Name}' data-description='${el.Description}' data-img='${el.ImgPath}' >Go somewhere</a>
-                </div>
-        </div>
-       `
+            <div class="card wow zoomIn" data-wow-duration="2s" >
+                <img class="card-img-top " src="${el.ImgPath}" alt="img">
+                <div class="card-body ">
+                    <h5 class="card-title ">${el.Name}</h5>
+                    <p class="card-text ">${el.Description}</p>
+                    <a href="#" class="mybtn btn btn-primary" data-toggle="modal" data-target="#editModal" data-id=${el.Id} data-name='${el.Name}' data-description='${el.Description}' data-img='${el.ImgPath}' >Go somewhere</a>
+                    </div>
+            </div>
+           `
             // !! TAKE care of @data-name=${el.Name}@ and @data-name='${el.Name}'@  the difference in reading up to a blank in the first case. space after @data-name='${el.Name}'@ is very important
             // !! data-'all in lowercase'
         list.append(listEl)
@@ -50,6 +90,7 @@ async function GetAndWriteAllContent() {
 
     const BMenu = await Request("BMenu");
     await WriteBmenu(BMenu);
+    await WriteSubMenu(BMenu);
 
 
 }
